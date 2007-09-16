4fcd88c1f6fad80ee5bcee925b6e293a15fb160c
Burlex <Burlex@8ad44209-8f18-4ceb-bdfa-99d0be514da2>
2007-09-16 17:13:17 +0000

    * removed unused enums since people are crying about it
    git-svn-id: https://ascentemubackup.svn.sourceforge.net/svnroot/ascentemubackup/trunk@1525 8ad44209-8f18-4ceb-bdfa-99d0be514da2

--- src/game/ArenaTeam.h
+++ src/game/ArenaTeam.h
@@ -20,61 +20,6 @@
 #ifndef ASCENT_ARENATEAMS_H
 #define ASCENT_ARENATEAMS_H
 
-enum ArenaTeamCommandTypes
-{
-    ERR_ARENA_TEAM_CREATE_S                 = 0x00,
-    ERR_ARENA_TEAM_INVITE_SS                = 0x01,
-    //ERR_ARENA_TEAM_QUIT_S                   = 0x02,
-    ERR_ARENA_TEAM_QUIT_S                   = 0x03,
-    ERR_ARENA_TEAM_FOUNDER_S                = 0x0C  // need check, probably wrong...
-};
-
-enum ArenaTeamCommandErrors
-{
-    //ARENA_TEAM_PLAYER_NO_MORE_IN_ARENA_TEAM = 0x00,
-    ERR_ARENA_TEAM_INTERNAL                 = 0x01,
-    ERR_ALREADY_IN_ARENA_TEAM               = 0x02,
-    ERR_ALREADY_IN_ARENA_TEAM_S             = 0x03,
-    ERR_INVITED_TO_ARENA_TEAM               = 0x04,
-    ERR_ALREADY_INVITED_TO_ARENA_TEAM_S     = 0x05,
-    ERR_ARENA_TEAM_NAME_INVALID             = 0x06,
-    ERR_ARENA_TEAM_NAME_EXISTS_S            = 0x07,
-    ERR_ARENA_TEAM_LEADER_LEAVE_S           = 0x08,
-    ERR_ARENA_TEAM_PERMISSIONS              = 0x08,
-    ERR_ARENA_TEAM_PLAYER_NOT_IN_TEAM       = 0x09,
-    ERR_ARENA_TEAM_PLAYER_NOT_IN_TEAM_SS    = 0x0A,
-    ERR_ARENA_TEAM_PLAYER_NOT_FOUND_S       = 0x0B,
-    ERR_ARENA_TEAM_NOT_ALLIED               = 0x0C
-};
-
-enum ArenaTeamEvents
-{
-    ERR_ARENA_TEAM_JOIN_SS                  = 3,    // player name  arena team name
-    ERR_ARENA_TEAM_LEAVE_SS                 = 4,    // player name  arena team name
-    ERR_ARENA_TEAM_REMOVE_SSS               = 5,    // player name  arena team name  captain name
-    ERR_ARENA_TEAM_LEADER_IS_SS             = 6,    // player name  arena team name
-    ERR_ARENA_TEAM_LEADER_CHANGED_SSS       = 7,    // old captain  new captain  arena team name
-    ERR_ARENA_TEAM_DISBANDED_S              = 8     // captain name  arena team name
-};
-
-/*
-need info how to send these ones:
-ERR_ARENA_TEAM_YOU_JOIN_S - client show it automatically when accept invite
-ERR_ARENA_TEAM_TARGET_TOO_LOW_S
-ERR_ARENA_TEAM_TOO_MANY_MEMBERS_S
-ERR_ARENA_TEAM_LEVEL_TOO_LOW_I
-*/
-
-enum ArenaTeamStatTypes
-{
-    STAT_TYPE_RATING    = 0,
-    STAT_TYPE_GAMES     = 1,
-    STAT_TYPE_WINS      = 2,
-    STAT_TYPE_PLAYED    = 3,
-    STAT_TYPE_WINS2     = 4,
-    STAT_TYPE_RANK      = 5
-};
-
 struct ArenaTeamMember
 {
 	PlayerInfo * Info;
