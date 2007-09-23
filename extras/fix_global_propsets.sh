70c1fe05ffee8bfc57ca28f951a331e4c397046e
Aim <Aim@8ad44209-8f18-4ceb-bdfa-99d0be514da2>
2007-09-23 17:27:13 +0000

    o added .libs and .a to global ignore o also fixed some eol-styles o added eol-style fixer in fix_global_propsets.sh
    git-svn-id: https://ascentemubackup.svn.sourceforge.net/svnroot/ascentemubackup/trunk@1627 8ad44209-8f18-4ceb-bdfa-99d0be514da2

--- extras/fix_global_propsets.sh
+++ extras/fix_global_propsets.sh
@@ -12,6 +12,7 @@ fi
 find . -type d | \
 	grep -v '.svn' | \
 	grep -v '.deps' | \
+	grep -v '.libs' | \
 	grep -v '^./win' | \
 	grep -v '^./extras' | \
 	grep -v 'autom4te.cache' | \
@@ -21,4 +22,42 @@ find . -type d | \
 cat .svn.ignore .svn.ignore.global > .svn.root.ignore
 svn propset svn:ignore -F .svn.root.ignore .
 rm .svn.root.ignore
+
+# Also add the 2 generated binaries.
+cat .svn.ignore.global > .svn.ascent.ignore
+echo "ascent" >> .svn.ascent.ignore
+svn propset svn:ignore -F .svn.ascent.ignore src/ascent/.
+rm .svn.ascent.ignore
+
+cat .svn.ignore.global > .svn.logonserver.ignore
+echo "logonserver" >> .svn.logonserver.ignore
+svn propset svn:ignore -F .svn.logonserver.ignore src/logonserver/.
+rm .svn.logonserver.ignore
+
+# Now fixing the eol-style's 
+echo "Updating eol-styles"
+
+NEWLINE='
+'
+IFS=$NEWLINE
+
+for i in `find . \
+  -name "*.h" -o \
+  -name "*.cpp" -o \
+  -name "Makefile.am" -o \
+  -name "configure.ac" -o \
+  -name "*.txt" -o \
+  -name "*.sql" -o \
+  -name "*.conf" -o \
+  -name "*.gm" -o \
+  -name "*.patch" -o \
+  -name "*.html"`; do
+        EOLSTYLE=$(svn pg svn:eol-style $i)
+        if [ $? -eq 0 ] && [ -z "$EOLSTYLE" ] ; then
+		# first remove the returns just incase. (fucking windows)
+		tr -d '\r' < $i > ${i}.tmp 
+		mv ${i}.tmp $i
+		svn ps svn:eol-style native $i 
+        fi
+done
 
