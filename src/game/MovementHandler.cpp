aca81f4821200ce297211f5fe1afd546433a6eef
burlex <burlex@8ad44209-8f18-4ceb-bdfa-99d0be514da2>
2007-10-23 13:05:56 +0000

    * argh another fix here
    git-svn-id: https://ascentemubackup.svn.sourceforge.net/svnroot/ascentemubackup/trunk@2035 8ad44209-8f18-4ceb-bdfa-99d0be514da2

--- src/game/MovementHandler.cpp
+++ src/game/MovementHandler.cpp
@@ -97,8 +97,11 @@ void WorldSession::HandleMoveTeleportAckOpcode( WorldPacket & recv_data )
 
 		if(GetPlayer()->GetSummon() != NULL)		// move pet too
 			GetPlayer()->GetSummon()->SetPosition((GetPlayer()->GetPositionX() + 2), (GetPlayer()->GetPositionY() + 2), GetPlayer()->GetPositionZ(), float(M_PI));
-		_player->m_position = _player->m_sentTeleportPosition;
-		_player->m_sentTeleportPosition.ChangeCoords(999999.0f,999999.0f,999999.0f);
+		if(_player->m_sentTeleportPosition.x != 999999.0f)
+		{
+			_player->m_position = _player->m_sentTeleportPosition;
+			_player->m_sentTeleportPosition.ChangeCoords(999999.0f,999999.0f,999999.0f);
+		}
 	}
 
 }
