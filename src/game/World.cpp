958ed33a96f722e61ee70d3026ae8913d5571f30
zack <zack@8ad44209-8f18-4ceb-bdfa-99d0be514da2>
2007-10-30 08:51:25 +0000

    Dragonspine Trophy, Romulo's Poison Vial, Madness of the Betrayer proc fix patch from Crunk
    git-svn-id: https://ascentemubackup.svn.sourceforge.net/svnroot/ascentemubackup/trunk@2146 8ad44209-8f18-4ceb-bdfa-99d0be514da2

--- src/game/World.cpp
+++ src/game/World.cpp
@@ -1014,6 +1014,8 @@ bool World::SetInitialWorldSettings()
 						pr|=PROC_ON_RESIST_VICTIM;
 					if(strstr(desc,"Your Shadow Word: Pain, Mind Flay and Vampiric Touch spells also cause the target"))
 						pr|=PROC_ON_CAST_SPELL;
+					if(strstr(desc,"your melee and ranged attacks"))
+						pr|=PROC_ON_MELEE_ATTACK|PROC_ON_RANGED_ATTACK;
 //					if(strstr(desc,"chill effect to your Blizzard"))
 //						pr|=PROC_ON_CAST_SPELL;	
 					//////////////////////////////////////////////////
