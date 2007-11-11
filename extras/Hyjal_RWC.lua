c4965925ad2b2346d099191b4aba0d2833d37af6
shady <shady@8ad44209-8f18-4ceb-bdfa-99d0be514da2>
2007-11-11 00:48:38 +0000

    *just updated example script. Its still not usable on live serverS!!!!
    git-svn-id: https://ascentemubackup.svn.sourceforge.net/svnroot/ascentemubackup/trunk@2282 8ad44209-8f18-4ceb-bdfa-99d0be514da2

--- extras/Hyjal_RWC.lua
+++ extras/Hyjal_RWC.lua
@@ -1,97 +1,85 @@
-//=====================================================================
-//I                  Rage Winterchill challenge                       I
-//=====================================================================
+RWC_RWC_ICEBOLT_TIMER = 9000
+RWC_RWC_FROSTNOVA_TIMER = 15000
+RWC_RWC_ICEARMOR_TIMER = 23000
+RWC_RWC_DEATHANDDECAY_TIMER = 11000
+RWC_ABOMINATION_POISONCLOUD_TIMER = 20000
+RWC_NECROMANCER_UNHOLYFRENZY_TIMER = 8000
+RWC_NECROMANCER_CRIPPLE_TIMER = 7000
+RWC_NECROMANCER_SUMMONSKELETONS_TIMER =20000
+RWC_NECROMANCER_SHADOWBOLT_TIMER = 5000
+RWC_CRYPTFIEND_WEB_TIMER = 13500
 
-//=========================== Config ==================================
-global RWC_RWC_ICEBOLT_TIMER = 10000
-global RWC_RWC_FROSTNOVA_TIMER = 15000
-global RWC_RWC_ICEARMOR_TIMER = 25000
-global RWC_RWC_DEATHANDDECAY_TIMER = 13000
-global RWC_ABOMINATION_POISONCLOUD_TIMER = 20000
-global RWC_NECROMANCER_UNHOLYFRENZY_TIMER = 10000
-global RWC_NECROMANCER_CRIPPLE_TIMER = 15000
-global RWC_NECROMANCER_SUMMONSKELETONS_TIMER = 40000
-global RWC_NECROMANCER_SHADOWBOLT_TIMER = 12500
-global RWC_CRYPTFIEND_WEB_TIMER = 10000
-
-
-
-
-//=========================== Implementation ==========================
-//------------------THE BOSS---------------
-global Jaina = null
-global DeathAndDecayAllowed = 1
+Jaina = null
+RWC_IsInProgress = 0
+RWC_DeathAndDecayAllowed = 1
 
 function RWC_RWC(unit)
-    print "MH:RWC_RWC init"
-    unit:MoveTo(5042.476074,-1786.176147,1321.710205,0)
+    unit:RegisterEvent("RWC_Waves_Move",10000,1)
 end
 
 function RWC_RWC_Combat(unit)
-    print "MH:RWC combat"
     unit:RegisterEvent("RWC_RWC_Icebolt",RWC_RWC_ICEBOLT_TIMER,0)
     unit:RegisterEvent("RWC_RWC_FrostNova",RWC_RWC_FROSTNOVA_TIMER,0)
     unit:RegisterEvent("RWC_RWC_IceArmor",RWC_RWC_ICEARMOR_TIMER,0)
+    unit:RegisterEvent("RWC_RWC_DeathAndDecay",RWC_RWC_DEATHANDDECAY_TIMER ,0)
 end
 
 function RWC_RWC_Icebolt(unit)
-    print "MH:RWC icebolt"
-    local plr = unit:GetRandomPlayer()
-    if (plr!=null)
-        unit:CastSpellOnTarget(31249,plr)
+    local plr = unit:GetRandomPlayer(0)
+    if (plr ~= nil) then
+        unit:FullCastSpellOnTarget(31249,plr)
+    end
 end
 
 
 function RWC_RWC_FrostNova(unit)
-    print "MH:RWC forstnova"
-    unit:CastSpell(32365)
-    DeathAndDecayAllowed = 0
+    unit:FullCastSpell(32365)
+    RWC_DeathAndDecayAllowed = 0
     unit:RegisterEvent("RWC_Allow_DeathAndDecay",6000,1)
 end
 
 function RWC_Allow_DeathAndDecay(unit)
-    DeathAndDecayAllowed = 0
+    RWC_DeathAndDecayAllowed = 1
 end
 
 function RWC_RWC_IceArmor(unit)
-    print "MH:RWC icearmor"
-    unit:CastSpell(31256)
+    unit:FullCastSpell(31256)
 end
 
 function RWC_RWC_DeathAndDecay(unit)
-    print "MH:RWC death&decay"
-    if (RWCNovaCooldown==0)
-         unit:CastSpell(34642)
+    if (RWC_DeathAndDecayAllowed>0) then
+         local plr = unit:GetRandomPlayer(0)
+         if (plr ~= nil) then
+             unit:FullCastSpellOnTarget(34642,plr)
+         end
+    end
 end
 
-//------------------Adds-------------------
 function RWC_Ghoul(unit)
-    print "MH:RWC_A init"
-    unit:MoveTo(5042.476074,-1786.176147,1321.710205,0)
+    unit:RegisterEvent("RWC_Waves_Move",10000,1)
 end
 
 function RWC_Abomination(unit)
-    print "MH:RWC_A init"
-    unit:MoveTo(5042.476074,-1786.176147,1321.710205,0)
+    unit:RegisterEvent("RWC_Waves_Move",10000,1)
 end
 
+function RWC_Waves_Move(unit)
+    unit:CreateWaypoint(5042.476074,-1786.176147,1321.710205,0,0,0,0)
+    unit:MoveToWaypoint(0)
+end
 function RWC_Abomination_Combat(unit)
-    print "MH:RWC_A combat"
-    unit:RegisterEvent("RWC_Abomination_PoisonCloud",RWC_ABOMINATION_POISONCOUD_TIMER,0)
+    unit:RegisterEvent("RWC_Abomination_PoisonCloud",RWC_ABOMINATION_POISONCLOUD_TIMER,0)
 end
 
 function RWC_Abomination_PoisonCloud(unit)
-    print "MH:RWC_A poison cloud"
     unit:FullCastSpell(30914)
 end
 
 function RWC_Necromancer(unit)
-    print "MH:RWC_N init"
-    unit:MoveTo(5042.476074,-1786.176147,1321.710205,0)
+    unit:RegisterEvent("RWC_Waves_Move",10000,1)
 end
 
 function RWC_Necromancer_Combat(unit)
-    print "MH:RWC_N combat"
     unit:RegisterEvent("RWC_Necromancer_UnholyFrenzy",RWC_NECROMANCER_UNHOLYFRENZY_TIMER,0)
     unit:RegisterEvent("RWC_Necromancer_SummonSkeletons",RWC_NECROMANCER_SUMMONSKELETONS_TIMER,0)
     unit:RegisterEvent("RWC_Necromancer_Cripple",RWC_NECROMANCER_CRIPPLE_TIMER,0)
@@ -99,49 +87,49 @@ function RWC_Necromancer_Combat(unit)
 end
 
 function RWC_Necromancer_UnholyFrenzy(unit)
-    print "MH:RWC_N unholy frenzy"
-    local plr = unit:GetRandomAlly()
-	if (plr != null)
-	    unit:FullCastSpellOnTarget(31626,plr)
+    local plr = unit:GetRandomFriend()
+    if (plr ~= nil) then
+        unit:FullCastSpellOnTarget(31626,plr)
+    end
+
 end    
 
 function RWC_Necromancer_SummonSkeletons(unit)
-    print "MH:RWC_N summonskeletons"
     unit:FullCastSpell(31617)
 end 
 
 function RWC_Necromancer_Cripple(unit)
-    print "MH:RWC_N unholy frenzy"
     local plr = unit:GetClosestPlayer();
-    if (plr != null)
-       unit:FullCastSpellOnTarget(33787,plr)
+    if (plr ~= nil) then
+        unit:FullCastSpellOnTarget(33787,plr)
+    end
 end   
 
 function RWC_Necromancer_ShadowBolt(unit)
-    print "MH:RWC_N shadowbolt"
-    unit:FullCastSpell(29487)
+    local plr = unit:GetClosestPlayer();
+    if (plr ~= nil) then
+        unit:FullCastSpellOnTarget(29487,plr)
+    end
+
 end    
 
 function RWC_CryptFiend(unit)
-    print "MH:RWC_CF init"
-    unit:MoveTo(5042.476074,-1786.176147,1321.710205,0)
+    unit:RegisterEvent("RWC_Waves_Move",10000,1)
 end
 
 function RWC_CryptFiend_Combat(unit)
-    print "MH:RWC_CF combat"
     unit:RegisterEvent("RWC_CryptFiend_Web",RWC_CRYPTFIEND_WEB_TIMER,0)
 end 
 
 function RWC_CryptFiend_Web(unit)
-    print "MH:RWC_CF web"
-    local plr = unit:GetRandomPlayer()
-    if (plr!=null)
+    local plr = unit:GetRandomPlayer(0)
+    if (plr ~= nil) then
         unit:FullCastSpellOnTarget(745,plr)
+    end
 end 
 
-//-------Weaves
-
 function RWC_Wave1()
+ print "RWC_Wave1"
  Jaina:SpawnCreature(17895,4955.778320,-1681.963013,1343.135986,0,1720,0)
  Jaina:SpawnCreature(17895,4955.778320,-1681.963013,1343.135986,0,1720,0)
  Jaina:SpawnCreature(17895,4955.778320,-1681.963013,1343.135986,0,1720,0)
@@ -156,6 +144,7 @@ function RWC_Wave1()
 end
 
 function RWC_Wave2()
+ print "RWC_Wave2"
  Jaina:SpawnCreature(17895,4955.778320,-1681.963013,1343.135986,0,1720,0)
  Jaina:SpawnCreature(17895,4955.778320,-1681.963013,1343.135986,0,1720,0)
  Jaina:SpawnCreature(17895,4955.778320,-1681.963013,1343.135986,0,1720,0)
@@ -172,6 +161,7 @@ function RWC_Wave2()
 end
 
 function RWC_Wave3()
+ print "RWC_Wave3"
  Jaina:SpawnCreature(17895,4955.778320,-1681.963013,1343.135986,0,1720,0)
  Jaina:SpawnCreature(17895,4955.778320,-1681.963013,1343.135986,0,1720,0)
  Jaina:SpawnCreature(17895,4955.778320,-1681.963013,1343.135986,0,1720,0)
@@ -188,6 +178,7 @@ function RWC_Wave3()
 end
 
 function RWC_Wave4()
+ print "RWC_Wave4"
  Jaina:SpawnCreature(17895,4955.778320,-1681.963013,1343.135986,0,1720,0)
  Jaina:SpawnCreature(17895,4955.778320,-1681.963013,1343.135986,0,1720,0)
  Jaina:SpawnCreature(17895,4955.778320,-1681.963013,1343.135986,0,1720,0)
@@ -205,6 +196,7 @@ function RWC_Wave4()
 end
 
 function RWC_Wave5()
+ print "RWC_Wave5"
  Jaina:SpawnCreature(17895,4955.778320,-1681.963013,1343.135986,0,1720,0)
  Jaina:SpawnCreature(17895,4955.778320,-1681.963013,1343.135986,0,1720,0)
  Jaina:SpawnCreature(17897,4955.778320,-1681.963013,1343.135986,0,1720,0)
@@ -221,6 +213,7 @@ function RWC_Wave5()
 end
 
 function RWC_Wave6()
+ print "RWC_Wave6"
  Jaina:SpawnCreature(17895,4955.778320,-1681.963013,1343.135986,0,1720,0)
  Jaina:SpawnCreature(17895,4955.778320,-1681.963013,1343.135986,0,1720,0)
  Jaina:SpawnCreature(17895,4955.778320,-1681.963013,1343.135986,0,1720,0)
@@ -237,6 +230,7 @@ function RWC_Wave6()
 end
 
 function RWC_Wave7()
+ print "RWC_Wave7"
  Jaina:SpawnCreature(17895,4955.778320,-1681.963013,1343.135986,0,1720,0)
  Jaina:SpawnCreature(17895,4955.778320,-1681.963013,1343.135986,0,1720,0)
  Jaina:SpawnCreature(17895,4955.778320,-1681.963013,1343.135986,0,1720,0)
@@ -253,6 +247,7 @@ function RWC_Wave7()
 end
 
 function RWC_Wave8()
+ print "RWC_Wave8"
  Jaina:SpawnCreature(17895,4955.778320,-1681.963013,1343.135986,0,1720,0)
  Jaina:SpawnCreature(17895,4955.778320,-1681.963013,1343.135986,0,1720,0)
  Jaina:SpawnCreature(17895,4955.778320,-1681.963013,1343.135986,0,1720,0)
@@ -271,27 +266,38 @@ function RWC_Wave8()
 end
 
 function RWC_Boss()
+ print "RWC_Boss"
  Jaina:SpawnCreature(17767,4955.778320,-1681.963013,1343.135986,0,1720,0)
 end
   
 function RWC_Start(unit)
- print "MH:RWC Start"
  Jaina = unit
- Jaina:RegisterEvent("RWC_Wave1",100,1)
+ if (RWC_IsInProgress == 0) then
+     print "MH:RWC Start"
+     local plr = Jaina:GetClosestPlayer()
+     if (plr~=nil) then
+	RWC_IsInProgress = plr:GetInstanceID()
+     end
+     Jaina:RegisterEvent("RWC_Wave1",100,1)
+ end
 end
 
 function RWC_Failed(unit)
  print "MH:RWC Failed"
+ Jaina:RemoveEvents()
+ Jaina:SendChatMessage(12,0,"Event Failed. Try once again and dont let me die")
+ Jaina:Despawn(0,5000)
+ RWC_IsInProgress = 0
 end
 
- RegisterUnitEvent(17895,6,"RWC_Ghoul")
- RegisterUnitEvent(17898,6,"RWC_Abomination")
- RegisterUnitEvent(17898,1,"RWC_Abomination_Combat")
- RegisterUnitEvent(17897,6,"RWC_CryptFiend")
- RegisterUnitEvent(17897,1,"RWC_CryptFiend_Combat")
- RegisterUnitEvent(17899,6,"RWC_Necromancer")
- RegisterUnitEvent(17899,1,"RWC_Necromancer_Combat")
- RegisterUnitEvent(17767,6,"RWC_RWC")
- RegisterUnitEvent(17767,1,"RWC_RWC_Combat")
+RegisterUnitEvent(17895,6,"RWC_Ghoul")
+RegisterUnitEvent(17898,6,"RWC_Abomination")
+RegisterUnitEvent(17898,1,"RWC_Abomination_Combat")
+RegisterUnitEvent(17897,6,"RWC_CryptFiend")
+RegisterUnitEvent(17897,1,"RWC_CryptFiend_Combat")
+RegisterUnitEvent(17899,6,"RWC_Necromancer")
+RegisterUnitEvent(17899,1,"RWC_Necromancer_Combat")
+RegisterUnitEvent(17767,6,"RWC_RWC")
+RegisterUnitEvent(17767,1,"RWC_RWC_Combat")
 RegisterUnitEvent(17772,10,"RWC_Start")
 RegisterUnitEvent(17772,4,"RWC_Failed")
