e0bf66fcf5e9144680050ad1467f97c619d7c0a6
burlex <burlex@8ad44209-8f18-4ceb-bdfa-99d0be514da2>
2007-11-12 04:34:25 +0000

    - w00t. forgot files >.>
    git-svn-id: https://ascentemubackup.svn.sourceforge.net/svnroot/ascentemubackup/trunk@2297 8ad44209-8f18-4ceb-bdfa-99d0be514da2

---
+++ src/game/EyeOfTheStorm.cpp
@@ -0,0 +1,104 @@
+/*
+ * Ascent MMORPG Server
+ * Copyright (C) 2005-2007 Ascent Team <http://www.ascentemu.com/>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+#include "StdAfx.h"
+
+EyeOfTheStorm::EyeOfTheStorm(MapMgr * mgr, uint32 id, uint32 lgroup, uint32 t) : CBattleground(mgr,id,lgroup,t)
+{
+
+}
+
+EyeOfTheStorm::~EyeOfTheStorm()
+{
+
+}
+
+bool EyeOfTheStorm::HookHandleRepop(Player * plr)
+{
+	return false;
+}
+
+void EyeOfTheStorm::HookOnAreaTrigger(Player * plr, uint32 id)
+{
+
+}
+
+void EyeOfTheStorm::HookOnPlayerDeath(Player * plr)
+{
+	plr->m_bgScore.Deaths++;
+	UpdatePvPData();
+}
+
+void EyeOfTheStorm::HookFlagDrop(Player * plr, GameObject * obj)
+{
+
+}
+
+void EyeOfTheStorm::HookFlagStand(Player * plr, GameObject * obj)
+{
+
+}
+
+void EyeOfTheStorm::HookOnMount(Player * plr)
+{
+
+}
+
+void EyeOfTheStorm::OnAddPlayer(Player * plr)
+{
+
+}
+
+void EyeOfTheStorm::OnRemovePlayer(Player * plr)
+{
+
+}
+
+void EyeOfTheStorm::OnCreate()
+{
+
+}
+
+void EyeOfTheStorm::HookOnPlayerKill(Player * plr, Unit * pVictim)
+{
+	plr->m_bgScore.KillingBlows++;
+	UpdatePvPData();
+}
+
+void EyeOfTheStorm::HookOnHK(Player * plr)
+{
+	plr->m_bgScore.HonorableKills++;
+	UpdatePvPData();
+}
+
+void EyeOfTheStorm::SpawnBuff(uint32 x)
+{
+
+}
+
+LocationVector EyeOfTheStorm::GetStartingCoords(uint32 Team)
+{
+	return LocationVector(0,0,0,0);
+}
+
+void EyeOfTheStorm::OnStart()
+{
+
+}
---
+++ src/game/EyeOfTheStorm.h
@@ -0,0 +1,49 @@
+/*
+ * Ascent MMORPG Server
+ * Copyright (C) 2005-2007 Ascent Team <http://www.ascentemu.com/>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+#ifndef _EOTS_H
+#define _EOTS_H
+
+class EyeOfTheStorm : public CBattleground
+{
+	EyeOfTheStorm(MapMgr * mgr, uint32 id, uint32 lgroup, uint32 t);
+	~EyeOfTheStorm();
+
+	void HookOnPlayerDeath(Player * plr);
+	void HookFlagDrop(Player * plr, GameObject * obj);
+	void HookFlagStand(Player * plr, GameObject * obj);
+	void HookOnMount(Player * plr);
+	void HookOnAreaTrigger(Player * plr, uint32 id);
+	bool HookHandleRepop(Player * plr);
+	void OnAddPlayer(Player * plr);
+	void OnRemovePlayer(Player * plr);
+	void OnCreate();
+	void HookOnPlayerKill(Player * plr, Unit * pVictim);
+	void HookOnHK(Player * plr);
+	void SpawnBuff(uint32 x);
+	LocationVector GetStartingCoords(uint32 Team);
+	static CBattleground * Create(MapMgr * m, uint32 i, uint32 l, uint32 t) { return new ArathiBasin(m, i, l, t); }
+
+	const char * GetName() { return "Eye of the Storm"; }
+	void OnStart();
+
+};
+
+#endif		// _EOTS_H
