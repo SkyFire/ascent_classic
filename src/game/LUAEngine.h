6451eedf46027a555b01c6d4e994b9cae990d45a
burlex <burlex@8ad44209-8f18-4ceb-bdfa-99d0be514da2>
2007-12-19 01:23:30 +0000

    * DO NOT UNCOMMENT THE STUFF THAT DISABLES LUA BY DEFAULT. IF I SEE ONE MORE COMMIT LIKE THIS I WILL REMOVE LUA ENTIRELY.
    git-svn-id: https://ascentemubackup.svn.sourceforge.net/svnroot/ascentemubackup/trunk@2594 8ad44209-8f18-4ceb-bdfa-99d0be514da2

--- src/game/LUAEngine.h
+++ src/game/LUAEngine.h
@@ -17,6 +17,21 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
+
+#ifndef __LUAENGINE_H
+#define __LUAENGINE_H
+
+#ifndef ENABLE_LUA_SCRIPTING
+
+/** Macros for calling lua-based events
+ */
+#define LUA_ON_UNIT_EVENT(unit,eventtype,miscunit,misc) 
+#define LUA_ON_QUEST_EVENT(plr,quest,eventtype,miscobject)
+#define LUA_ON_GO_EVENT(go,evtype,miscunit) 
+#define LUA_CALL_FUNC(unit,funcname) 
+
+#else
+
 /** Macros for calling lua-based events
  */
 #define LUA_ON_UNIT_EVENT(unit,eventtype,miscunit,misc) if(unit->GetTypeId()==TYPEID_UNIT && unit->IsInWorld()) { unit->GetMapMgr()->GetScriptEngine()->OnUnitEvent(unit,eventtype,miscunit,misc); }
@@ -144,3 +159,7 @@ public:
 
 	void ReloadScripts();
 };
+
+#endif
+#endif
+
