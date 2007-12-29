6aef13d199e28df3012862c2ba9442eea9596668
debug <debug@8ad44209-8f18-4ceb-bdfa-99d0be514da2>
2007-12-29 02:52:21 +0000

    item localization was working and until someone tried to do it no one realized item_pages and quest localization tables where not setup they would just about work for EFIGS but nothing else
    git-svn-id: https://ascentemubackup.svn.sourceforge.net/svnroot/ascentemubackup/trunk@2759 8ad44209-8f18-4ceb-bdfa-99d0be514da2

---
+++ sql/world_updates/2750_localization_fixes.sql
@@ -0,0 +1,7 @@
+LOCK TABLES `quests_localized` WRITE;
+ALTER TABLE `quests_localized` collate utf8_unicode_ci;
+UNLOCK TABLES;
+
+LOCK TABLES `item_pages_localized` WRITE;
+ALTER TABLE `item_pages_localized` collate utf8_unicode_ci;
+UNLOCK TABLES;
