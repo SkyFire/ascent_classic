dc2a4e7a543f5d506cd0bb968c242787b77a8a7f
debug <debug@8ad44209-8f18-4ceb-bdfa-99d0be514da2>
2007-12-29 15:03:23 +0000

    hah added the other tables that where broken and fixed itempages
    git-svn-id: https://ascentemubackup.svn.sourceforge.net/svnroot/ascentemubackup/trunk@2763 8ad44209-8f18-4ceb-bdfa-99d0be514da2

--- sql/world_updates/2750_localization_fixes.sql
+++ sql/world_updates/2750_localization_fixes.sql
@@ -1,7 +1,20 @@
+LOCK TABLES `creature_names_localized` WRITE;
+ALTER TABLE `creature_names_localized` collate utf8_unicode_ci;
+UNLOCK TABLES;
+
 LOCK TABLES `quests_localized` WRITE;
 ALTER TABLE `quests_localized` collate utf8_unicode_ci;
 UNLOCK TABLES;
 
-LOCK TABLES `item_pages_localized` WRITE;
-ALTER TABLE `item_pages_localized` collate utf8_unicode_ci;
+LOCK TABLES `itempages_localized` WRITE;
+ALTER TABLE `itempages_localized` collate utf8_unicode_ci;
 UNLOCK TABLES;
+
+LOCK TABLES `npc_text_localized` WRITE;
+ALTER TABLE `npc_text_localized` collate utf8_unicode_ci;
+UNLOCK TABLES;
+
+LOCK TABLES `npc_text_localized` WRITE;
+ALTER TABLE `npc_text_localized` collate utf8_unicode_ci;
+UNLOCK TABLES;
+
