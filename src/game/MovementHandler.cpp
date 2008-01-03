d2db599c225378d0aa6fc5f660d3c6a46e37bd14
debug <debug@8ad44209-8f18-4ceb-bdfa-99d0be514da2>
2008-01-03 21:29:56 +0000

    oops
    git-svn-id: https://ascentemubackup.svn.sourceforge.net/svnroot/ascentemubackup/trunk@2842 8ad44209-8f18-4ceb-bdfa-99d0be514da2

--- src/game/MovementHandler.cpp
+++ src/game/MovementHandler.cpp
@@ -571,20 +571,20 @@ void WorldSession::HandleMovementOpcodes( WorldPacket & recv_data )
 				case 2:
 					{
 						sChatHandler.SystemMessage( this, "Speedhack detected this is your first warning. Your account has been flagged for later processing by server administrators. You will be unrooted in 5 seconds.");
+						sCheatLog.writefromsession( this, "Speedhack first warning,delta of %g", distance_delta );
 						_player->SetMovement( MOVE_ROOT, 1 );
 						sEventMgr.AddEvent( _player, &Player::SetMovement, uint8(MOVE_UNROOT), uint32(1), EVENT_DELETE_TIMER, 5000, 1, EVENT_FLAG_DO_NOT_EXECUTE_IN_WORLD_CONTEXT );
 						_player->ResetHeartbeatCoords();
 						_player->m_speedhackChances--;
-						sCheatLog.writefromsession( this, "Speedhack first warning,delta of %g", distance_delta );
 					}
 				case 1:
 					{
 						sChatHandler.SystemMessage( this, "Speedhack detected this is your second warning. Your account has been flagged for later processing by server administrators. You will be unrooted in 30 seconds.");
+						sCheatLog.writefromsession( this, "Speedhack second warning, delta of %g", distance_delta );
 						_player->SetMovement( MOVE_ROOT, 1 );
 						sEventMgr.AddEvent( _player, &Player::SetMovement, uint8(MOVE_UNROOT), uint32(1), EVENT_DELETE_TIMER, 30000, 1, EVENT_FLAG_DO_NOT_EXECUTE_IN_WORLD_CONTEXT );
 						_player->ResetHeartbeatCoords();
 						_player->m_speedhackChances--;
-						sCheatLog.writefromsession( this, "Speedhack second warning, delta of %g", distance_delta );
 					}
 				case 0:
 					{
