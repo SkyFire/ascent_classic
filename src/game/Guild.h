23b33dab53f70ead9bcacf34507a7e41aae90322
supalosa <supalosa@8ad44209-8f18-4ceb-bdfa-99d0be514da2>
2008-01-09 00:21:57 +0000

    - Reverted past two revisions, before I get yelled at for a) exposing the m_members.begin and .end, and b) reverting that and almost exposing m_members as a const public D:
    git-svn-id: https://ascentemubackup.svn.sourceforge.net/svnroot/ascentemubackup/trunk@3003 8ad44209-8f18-4ceb-bdfa-99d0be514da2

--- src/game/Guild.h
+++ src/game/Guild.h
@@ -501,10 +501,6 @@ public:
 		m_lock.Release();
 		return ret;
 	}
-	/** Gets members */
-	typedef map<PlayerInfo*, GuildMember*> GuildMemberMap;
-	ASCENT_INLINE GuildMemberMap::iterator GetGuildMembersBegin() { return m_members.begin(); }
-	ASCENT_INLINE GuildMemberMap::iterator GetGuildMembersEnd() { return m_members.end(); }
 
 	/** Sends the guild bank to this client.
 	 */
@@ -548,7 +544,7 @@ protected:
 	
 	/** Guild Member Map.
 	 */
-
+	typedef map<PlayerInfo*, GuildMember*> GuildMemberMap;
 	GuildMemberMap m_members;
 
 	/** Guild Rank Information.
