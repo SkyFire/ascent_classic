3a9a2b9f4865dd6890ddbfdfe27d517b1111021a
debug <debug@8ad44209-8f18-4ceb-bdfa-99d0be514da2>
2008-02-12 17:12:34 +0000

    fall dmg detection tweak
    git-svn-id: https://ascentemubackup.svn.sourceforge.net/svnroot/ascentemubackup/trunk@3851 8ad44209-8f18-4ceb-bdfa-99d0be514da2

--- src/ascent-world/MovementHandler.cpp
+++ src/ascent-world/MovementHandler.cpp
@@ -410,7 +410,7 @@ void WorldSession::HandleMovementOpcodes( WorldPacket & recv_data )
 
 		if( sWorld.antihack_falldmg )
 		{
-			if( !_player->bFeatherFall && ( !_player->blinked || _player->m_redirectCount > 8 ) && !_player->flying_aura && !_player->m_TransporterGUID && _player->_lastHeartbeatZ - movement_info.z > 2.5f && !( movement_info.flags & MOVEFLAG_FULL_FALLING_MASK ) )
+			if( !_player->bFeatherFall && ( !_player->blinked || _player->m_redirectCount > 4 ) && !_player->flying_aura && !_player->m_TransporterGUID && _player->_lastHeartbeatZ - movement_info.z > 2.0f && !( movement_info.flags & MOVEFLAG_FULL_FALLING_MASK ) )
 			{
 				_player->m_heightDecreaseCount++;
 				if( _player->m_heightDecreaseCount > 16.0f )
