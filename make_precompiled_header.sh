88f7cf29c6c0c81ad4d64923c5e65d0bd88a8511
debug <debug@8ad44209-8f18-4ceb-bdfa-99d0be514da2>
2008-02-15 16:00:47 +0000

    front ported * script fix
    git-svn-id: https://ascentemubackup.svn.sourceforge.net/svnroot/ascentemubackup/trunk@3877 8ad44209-8f18-4ceb-bdfa-99d0be514da2

--- make_precompiled_header.sh
+++ make_precompiled_header.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 echo Building Precompiled Header...
+
 cd src/ascent-world
-g++ -DHAVE_CONFIG_H -I. -I. -I../.. -I/usr/include/mysql -I/usr/include/openssl -I/usr/local/include/openssl -I/usr/local/include -I. -I./../../dep/include  -I./../ascent-shared -I./../../dep/src -g -O2 -o StdAfx.h.gch StdAfx.h
+#g++ -DHAVE_CONFIG_H -I. -I. -I../.. -I/usr/include/mysql -I/usr/include/openssl -I/usr/local/include/openssl -I/usr/local/include -I. -I./../../dep/include  -I./../ascent-shared -I./../../dep/src -g -O2 -o StdAfx.h.gch StdAfx.h
+g++ -DHAVE_CONFIG_H -I. -I../.. -I/usr/include/mysql -I/usr/include/openssl -I/usr/local/include/openssl -I/usr/local/include -I. -I./../../dep/include -I./../ascent-shared -I./../../dep/src -g -O2 -o StdAfx.h.gch StdAfx.h
 
 echo Symlinking script gch files.
 cd ../scripts/src/GossipScripts
@@ -22,5 +24,3 @@ rm -f StdAfx.h.gch
 ln -s ../../../ascent-world/StdAfx.h.gch ./StdAfx.h.gch
 
 echo Done. Now you can run make, and experience much faster build times. Remember to re-run this script every time you svn update.
-
-
