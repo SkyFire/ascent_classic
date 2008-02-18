9c573ac203c397f41809a195ad6ed6b33d8e08b7
debug <debug@8ad44209-8f18-4ceb-bdfa-99d0be514da2>
2008-02-18 13:53:24 +0000

    ...
    git-svn-id: https://ascentemubackup.svn.sourceforge.net/svnroot/ascentemubackup/trunk@3929 8ad44209-8f18-4ceb-bdfa-99d0be514da2

--- src/ascent-world/Spell.cpp
+++ src/ascent-world/Spell.cpp
@@ -3603,7 +3603,7 @@ exit:
 		if( m_spellInfo->maxLevel && u_caster->getLevel() > m_spellInfo->maxLevel )
 			diff += m_spellInfo->maxLevel;
 		else
-			diff += u_caster->getLevel();	
+			diff += u_caster->getLevel();
 
 		randomPoints += float2int32( diff * randomPointsPerLevel );
 		basePoints += float2int32( diff * basePointsPerLevel );
