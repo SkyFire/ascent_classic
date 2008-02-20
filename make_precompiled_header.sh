a4114a85bb71c689d28c451159f4f3ff10ce97cf
debug <debug@8ad44209-8f18-4ceb-bdfa-99d0be514da2>
2008-02-20 01:32:23 +0000

    postgres stuff
    git-svn-id: https://ascentemubackup.svn.sourceforge.net/svnroot/ascentemubackup/trunk@3960 8ad44209-8f18-4ceb-bdfa-99d0be514da2

--- make_precompiled_header.sh
+++ make_precompiled_header.sh
@@ -3,8 +3,8 @@
 echo Building Precompiled Header...
 
 cd src/ascent-world
-#g++ -DHAVE_CONFIG_H -I. -I. -I../.. -I/usr/include/mysql -I/usr/include/openssl -I/usr/local/include/openssl -I/usr/local/include -I. -I./../../dep/include  -I./../ascent-shared -I./../../dep/src -g -O2 -o StdAfx.h.gch StdAfx.h
-g++ -DHAVE_CONFIG_H -I. -I../.. -I/usr/include/mysql -I/usr/include/openssl -I/usr/local/include/openssl -I/usr/local/include -I. -I./../../dep/include -I./../ascent-shared -I./../../dep/src -g -O2 -o StdAfx.h.gch StdAfx.h
+#g++ -DHAVE_CONFIG_H -I. -I. -I../.. -I/usr/include/mysql -I/usr/include/mysql -I/usr/include/openssl -I/usr/local/include/openssl -I/usr/local/include -I. -I./../../dep/include  -I./../ascent-shared -I./../../dep/src -g -O2 -o StdAfx.h.gch StdAfx.h
+g++ -DHAVE_CONFIG_H -I. -I../.. -I/usr/include/mysql -I/usr/local/include/mysql -I/usr/include/postgressql -I/usr/local/include/postgressql -I/usr/include/openssl -I/usr/local/include/openssl -I/usr/local/include -I. -I./../../dep/include -I./../ascent-shared -I./../../dep/src -g -O2 -o StdAfx.h.gch StdAfx.h
 
 echo Symlinking script gch files.
 cd ../scripts/src/GossipScripts
