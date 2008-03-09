f8c51e8306758441bee566af8e324eb52e18e2df
burlex <burlex@8ad44209-8f18-4ceb-bdfa-99d0be514da2>
2008-03-09 17:15:12 +0000

    * tag file, saves reubilding the extractor every time. i'll fix the windows one later
    git-svn-id: https://ascentemubackup.svn.sourceforge.net/svnroot/ascentemubackup/trunk@4128 8ad44209-8f18-4ceb-bdfa-99d0be514da2

--- extract_svn_revision.h
+++ extract_svn_revision.h
@@ -9,7 +9,7 @@ echo "" >> svn_revision.h
 echo "#ifndef SVN_REVISION_H" >> svn_revision.h
 echo "#define SVN_REVISION_H" >> svn_revision.h
 echo "" >> svn_revision.h
-echo "static const char * BUILD_TAG = \"4.0-STABLE\";" >> svn_revision.h
+echo "static const char * BUILD_TAG = \"`cat ascent-tag`\";" >> svn_revision.h
 echo "static int BUILD_REVISION =" `svn info | grep Revision | cut -d' ' -f2`";" >> svn_revision.h
 echo "" >> svn_revision.h
 echo "#endif         // SVN_REVISION_H" >> svn_revision.h
