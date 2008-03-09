2ec6f4b9a6e397daa3dc1eeaa872417825876599
burlex <burlex@8ad44209-8f18-4ceb-bdfa-99d0be514da2>
2008-03-09 17:00:38 +0000

    * and these
    git-svn-id: https://ascentemubackup.svn.sourceforge.net/svnroot/ascentemubackup/trunk@4124 8ad44209-8f18-4ceb-bdfa-99d0be514da2

--- make_precompiled_header_debug.sh
+++ make_precompiled_header_debug.sh
@@ -2,24 +2,7 @@
 
 echo Building Precompiled Header...
 cd src/ascent-world
-g++ -g -DHAVE_CONFIG_H -I. -I../.. -I/usr/include/mysql -I/usr/include/openssl -I/usr/local/include/openssl -I/usr/local/include -I. -I./../../dep/include -I./../ascent-shared -I./../../dep/src -o StdAfx.h.gch StdAfx.h
-
-echo Symlinking script gch files.
-cd ../scripts/src/GossipScripts
-rm -f StdAfx.h.gch
-ln -s ../../../ascent-world/StdAfx.h.gch ./StdAfx.h.gch
-
-cd ../InstanceScripts
-rm -f StdAfx.h.gch
-ln -s ../../../ascent-world/StdAfx.h.gch ./StdAfx.h.gch
-
-cd ../SpellHandlers
-rm -f StdAfx.h.gch
-ln -s ../../../ascent-world/StdAfx.h.gch ./StdAfx.h.gch
-
-cd ../ServerStatusPlugin
-rm -f StdAfx.h.gch
-ln -s ../../../ascent-world/StdAfx.h.gch ./StdAfx.h.gch
+g++ -g -DHAVE_CONFIG_H -I. -I../.. -I/usr/include/mysql -I/usr/include/openssl -I/usr/local/include/openssl -I/usr/local/include -I. -I./../ascent-shared -o StdAfx.h.gch StdAfx.h
 
 echo Done. Now you can run make, and experience much faster build times. Remember to re-run this script every time you svn update.
 
