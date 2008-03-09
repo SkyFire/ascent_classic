6ac29ba4071291073d0778de38aa604e52f2783d
burlex <burlex@8ad44209-8f18-4ceb-bdfa-99d0be514da2>
2008-03-09 17:51:39 +0000

    * applied: http://www.ascentemu.com/forums/index.php?showtopic=17294
    git-svn-id: https://ascentemubackup.svn.sourceforge.net/svnroot/ascentemubackup/trunk@4133 8ad44209-8f18-4ceb-bdfa-99d0be514da2

--- src/scripts/src/SpellHandlers/WarriorSpells.cpp
+++ src/scripts/src/SpellHandlers/WarriorSpells.cpp
@@ -112,8 +112,10 @@ bool Execute(uint32 i, Spell* pSpell)
     add_damage *= pSpell->u_caster->GetUInt32Value(UNIT_FIELD_POWER2) / 10;   // rage is *10 always
     
     // send spell damage log
-    pSpell->u_caster->SpellNonMeleeDamageLog(target, 20647, base_dmg + add_damage, false);
-    // zero rage
+	//pSpell->u_caster->SpellNonMeleeDamageLog(target, 20647, base_dmg + add_damage, false);
+	SpellEntry *sp_for_the_logs = dbcSpell.LookupEntry(20647);
+	pSpell->u_caster->Strike( target, MELEE, sp_for_the_logs, base_dmg + add_damage, 0, 0, true, true );
+	// zero rage
     pSpell->u_caster->SetUInt32Value(UNIT_FIELD_POWER2, 0);
     return true;
 }
