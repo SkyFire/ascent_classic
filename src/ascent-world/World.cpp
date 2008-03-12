0577d992bacbbf59c6b84fe6f365e6c808d4e08b
burlex <burlex@8ad44209-8f18-4ceb-bdfa-99d0be514da2>
2008-03-12 21:00:51 +0000

    * applied: http://www.ascentemu.com/forums/index.php?showtopic=17424
    git-svn-id: https://ascentemubackup.svn.sourceforge.net/svnroot/ascentemubackup/trunk@4182 8ad44209-8f18-4ceb-bdfa-99d0be514da2

--- src/ascent-world/World.cpp
+++ src/ascent-world/World.cpp
@@ -2582,6 +2582,27 @@ bool World::SetInitialWorldSettings()
 	if( sp != NULL )
 		sp->procFlags = PROC_ON_CRIT_HIT_VICTIM;
 
+	//Shaman - Shamanistic Focus
+	sp = dbcSpell.LookupEntryForced( 43338 );
+	if( sp != NULL )
+	{
+		sp->EffectApplyAuraName[0] = SPELL_AURA_PROC_TRIGGER_SPELL;
+		sp->procFlags = PROC_ON_CRIT_ATTACK;
+		sp->EffectTriggerSpell[0] = 43339;
+		sp->procChance = 100;
+		sp->maxstack = 1;
+	}
+
+	//Shaman focused
+	sp = dbcSpell.LookupEntryForced( 43339 );
+	if( sp != NULL )
+	{
+		sp->procFlags = PROC_ON_CAST_SPELL;
+		sp->EffectApplyAuraName[1] = SPELL_AURA_PROC_TRIGGER_SPELL;
+		sp->EffectMiscValue[0] = SMT_COST;
+		sp->EffectSpellGroupRelation[0] = 1048576 | 268435456 | 2147483648;
+	}
+
 	//Shaman - Focused Casting
 	sp = dbcSpell.LookupEntryForced( 29063 );
 	if( sp != NULL )
