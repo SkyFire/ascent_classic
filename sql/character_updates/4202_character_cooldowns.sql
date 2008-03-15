c67747bab13a2e09f69a20f249d539e30441d87a
burlex <burlex@8ad44209-8f18-4ceb-bdfa-99d0be514da2>
2008-03-15 00:31:13 +0000

    * corrected structure here
    git-svn-id: https://ascentemubackup.svn.sourceforge.net/svnroot/ascentemubackup/trunk@4205 8ad44209-8f18-4ceb-bdfa-99d0be514da2

--- sql/character_updates/4202_character_cooldowns.sql
+++ sql/character_updates/4202_character_cooldowns.sql
@@ -1,9 +1,12 @@
 DROP TABLE IF EXISTS playercooldownsecurity;
 DROP TABLE IF EXISTS playercooldownitems;
+DROP TABLE IF EXISTS playercooldowns;
 
 CREATE TABLE `playercooldowns` (
   `player_guid` int(30) NOT NULL,
   `cooldown_type` int(30) NOT NULL COMMENT '0 is spell, 1 is item, 2 is spell category',
   `cooldown_misc` int(30) NOT NULL COMMENT 'spellid/itemid/category',
-  `cooldown_expire_time` int(30) NOT NULL COMMENT 'expiring time in unix epoch format'
+  `cooldown_expire_time` int(30) NOT NULL COMMENT 'expiring time in unix epoch format',
+  `cooldown_spellid` int(30) NOT NULL COMMENT 'spell that cast it',
+  `cooldown_itemid` int(30) NOT NULL COMMENT 'item that cast it'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
