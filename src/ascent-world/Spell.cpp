7c4240ac724c10ba439c07e28d26ff030d514f05
burlex <burlex@8ad44209-8f18-4ceb-bdfa-99d0be514da2>
2008-03-19 00:16:44 +0000

    * GetUnit -> GetPlayer
    git-svn-id: https://ascentemubackup.svn.sourceforge.net/svnroot/ascentemubackup/trunk@4255 8ad44209-8f18-4ceb-bdfa-99d0be514da2

--- src/ascent-world/Spell.cpp
+++ src/ascent-world/Spell.cpp
@@ -3740,7 +3740,7 @@ exit:
 		else if( u_caster && u_caster->GetGUID() == i_caster->GetUInt64Value( ITEM_FIELD_CREATOR ) )
 			tcaster = u_caster;
 		else if( target->IsInWorld() )
-			tcaster = target->GetMapMgr()->GetUnit( i_caster->GetUInt64Value( ITEM_FIELD_CREATOR ) ); //we should inherit the modifiers from the conjured food caster
+			tcaster = target->GetMapMgr()->GetPlayer( i_caster->GetUInt32Value( ITEM_FIELD_CREATOR ) ); //we should inherit the modifiers from the conjured food caster
 	}
 
 	if( tcaster != NULL )
