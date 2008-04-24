9d8b1a36f7fed6513cb6991a3c084ec46953dab2
jajcer <jajcer@8ad44209-8f18-4ceb-bdfa-99d0be514da2>
2008-04-24 11:18:06 +0000

    *) Removed some useless stuff
    4400 is mine :D
    
    git-svn-id: https://ascentemubackup.svn.sourceforge.net/svnroot/ascentemubackup/trunk@4400 8ad44209-8f18-4ceb-bdfa-99d0be514da2

--- src/ascent-world/SpellEffects.cpp
+++ src/ascent-world/SpellEffects.cpp
@@ -4997,7 +4997,7 @@ void Spell::SpellEffectDispelMechanic(uint32 i)
 	{
 		if( unitTarget->m_auras[x] && !unitTarget->m_auras[x]->IsPositive())
 		{
-			if( unitTarget->m_auras[x]->GetSpellProto()->DispelType == sMisc || unitTarget->m_auras[x]->GetSpellProto()->EffectMechanic[x] == (int32)sMisc )
+			if( unitTarget->m_auras[x]->GetSpellProto()->DispelType == sMisc )
 				unitTarget->m_auras[x]->Remove();
 		}
 	}
