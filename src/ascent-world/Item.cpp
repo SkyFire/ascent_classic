f8e2025ebd109d793a03d501e20a9a31d9c7087e
supalosa <supalosa@8ad44209-8f18-4ceb-bdfa-99d0be514da2>
2008-05-26 07:36:15 +0000

    - Gems put into the head slot will no longer require re-equipping or relogging to have the stats applied.
    git-svn-id: https://ascentemubackup.svn.sourceforge.net/svnroot/ascentemubackup/trunk@4564 8ad44209-8f18-4ceb-bdfa-99d0be514da2

--- src/ascent-world/Item.cpp
+++ src/ascent-world/Item.cpp
@@ -601,7 +601,7 @@ int32 Item::AddEnchantment( EnchantEntry* Enchantment, uint32 Duration, bool Per
 	
 		/* Only apply the enchantment bonus if we're equipped */
 		uint8 slot = m_owner->GetItemInterface()->GetInventorySlotByGuid( GetGUID() );
-		if( slot > EQUIPMENT_SLOT_START && slot < EQUIPMENT_SLOT_END )
+		if( slot >= EQUIPMENT_SLOT_START && slot < EQUIPMENT_SLOT_END )
             ApplyEnchantmentBonus( Slot, APPLY );
 	}
 
